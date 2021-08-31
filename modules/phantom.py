3a74afe0deacd99e3a9663bd279a6202bdceda0d
James Cadman <cadman@roe.ac.uk>
2021-08-31 12:21:46 +0100

    added docstrings to functions in modules/phantom.py, as the file is getting bulky now.

--- modules/phantom.py
+++ modules/phantom.py
@@ -8,6 +8,9 @@ sys.path.insert(0,'../phantom_files')
 from libanalysis import PhantomAnalysis as pa
 
 def get_units():
+    """
+    Dictionary of code -> physical units conversion.
+    """
     units = {
         'udens' : 5.94103125029151e-07,
         'umass' : 1.9891e+33,
@@ -22,19 +25,36 @@ def get_units():
 UNITS = get_units()
 
 def read_dump_file(input_file):
+    """
+    Read phantom dump file using pyphantom/PhantomAnalysis
+    """
     disc = pa(input_file)
     return disc
 
 def folder_id(dir):
+    """
+    Convert directory path to a readable id which is generally used here to
+        generate output file names for figures, data files etc.
+    """
     return dir.split('../')[1].replace('/','_').replace('.','')
 
 def collect_all_dumps(parent_dir='..'):
+    """
+    Walk through all directories in parent_dir, collecting any folders containing a phantom
+        dump file with the prefix 'sgdisc_'
+    """
     all_dumps = [os.path.join(r,f) for r,d,files in os.walk(parent_dir) for f in files if 'sgdisc_' in f]
     unwanted_extensions = ['.png','.dat','.tmp','.ascii']
     all_dumps = [dump for dump in all_dumps if not any([dump.endswith(ext) for ext in unwanted_extensions])]
     return all_dumps
 
 def collect_all_final_dumps(parent_dir='..'):
+    """
+    Walk through all directories in parent_dir, collecting any folders containing a phantom
+        dump file with the prefix 'sgdisc_'. For each folder containing dump files, this function
+        will return the dump which represents the latest time in the simulation (identified by its
+        numerical suffix).
+    """
     all_final_dumps = []
     unwanted_extensions = ['.png','.dat','.tmp','.ascii']
     for r,d,files in os.walk('..'):
@@ -42,11 +62,17 @@ def collect_all_final_dumps(parent_dir='..'):
         all_dumps = [dump for dump in all_dumps if not any([dump.endswith(ext) for ext in unwanted_extensions])]
         # also don't want any dumps from the 'extra_dumps' subfolders
         all_dumps = [dump for dump in all_dumps if 'extra' not in dump]
+        # also don't want any dumps from the 'debugging_dumps' subfolders
+        all_dumps = [dump for dump in all_dumps if 'debugging' not in dump]
         if all_dumps:
             all_final_dumps.append(sorted(all_dumps)[-1])
     return all_final_dumps
 
 def edit_splash_limits(disc):
+    """
+    Edit splash.limits file for a few custom plotting preferences.
+    Required splash.defaults file is present in the working directory.
+    """
     # plots +/- 150au around the star
     star_xy = disc.ptmass_xyzmh[:2,0]
     xmin, xmax = (star_xy[0] - 200, star_xy[0] + 200)
@@ -71,6 +97,10 @@ def edit_splash_limits(disc):
     return
 
 def edit_splash_defaults():
+    """
+    Edit splash.defaults file for a few custom plotting preferences.
+    Required splash.defaults file is present in the working directory.
+    """
     splash_defaults = np.genfromtxt('splash.defaults',dtype=str,delimiter='1234./,;')
     output = splash_defaults.copy()
     # ensure plotting physical units
@@ -90,7 +120,13 @@ def edit_splash_defaults():
 DEFAULT_SPLASH_OUTPUT_FILE = 'splash.png'
 
 def generate_png_plot(input_file,output_file=DEFAULT_SPLASH_OUTPUT_FILE):
-
+    """
+    Generate rendered plot of gas density by running splash and saving the output
+        as a png file.
+    Function will copy splash.limits and splash.defaults file into the working
+        directory, edit them to contain the desired limits, and then generate the
+        png plot.
+    """
     input_dir = os.path.dirname(input_file)
 
     if os.path.exists(os.path.join(input_dir,'splash.limits')):
@@ -125,27 +161,26 @@ def display_splash_plot(image_file):
     plt.show()
 
 def get_midplane_mask(disc):
-    # get particles within 1 scale height of disc midplane (if z < H)
+    """
+    Get particles within 1 scale height of disc midplane (if z < H), from
+        phantom disc instance.
+    """
     mask = abs(disc.xyzh[2] - disc.ptmass_xyzmh[2,0]) <= abs(disc.xyzh[3])
     return mask
 
-def bin_azimuthally(rads,vals,nbins=100,rmax=100):
+def create_binary_separation_file(input_dir,output_dir):
+    """
+    Generate binary separation file of [ifile, time, binary separation] from a
+        directory of phantom dumps generated during a run containing bianry stars.
+    Also generates a file of [star1_x, star1_y, star1_z, star2_x, star2_y, star2_z]
+        for any other desired calculations.
+    Useful for plotting the phase of the binary orbit and monitoring the progress of
+        each simulation.
+    """
+    dir_id = folder_id(input_dir)
 
-    assert len(rads)==len(vals)
-
-    rad_bins = np.linspace(0,rmax,nbins)
-
-    ibins = np.digitize(rads,rad_bins)
-    binned_vals = np.asarray([np.mean(vals[ibins==bin]) for bin in range(nbins)])
-
-    return rad_bins,binned_vals
-
-def create_binary_separation_file(dir,out_dirname):
-
-    dir_id = folder_id(dir)
-
-    sepfile = open(os.path.join(out_dirname,dir_id+'_binsep.dat'), 'w')
-    posfile = open(os.path.join(out_dirname,dir_id+'_ptmass_xyz.dat'), 'w')
+    sepfile = open(os.path.join(output_dir,dir_id+'_binsep.dat'), 'w')
+    posfile = open(os.path.join(output_dir,dir_id+'_ptmass_xyz.dat'), 'w')
 
     dumpfiles = [file for file in os.listdir(dir) if 'sgdisc_00' in file]
     # exclude ascii or temp files
@@ -155,9 +190,9 @@ def create_binary_separation_file(dir,out_dirname):
 
     for dump in dumpfiles:
         try:
-            disc = read_dump_file(os.path.join(dir,dump))
+            disc = read_dump_file(os.path.join(input_dir,dump))
         except Exception as e:
-            print('failed to load dumpfile: %s' %os.path.join(dir,dump))
+            print('failed to load dumpfile: %s' %os.path.join(input_dir,dump))
             continue
         ptmass_xyzmh = disc.ptmass_xyzmh
         # only want to calculate anything if there's 2 stars
@@ -174,7 +209,9 @@ def create_binary_separation_file(dir,out_dirname):
     posfile.close()
 
 def get_az_averaged_properties(disc,nbins=100,rmax=100):
-
+    """
+    Calcualte azimuthally averaged disc properties from pyphantom disc instance.
+    """
     radii = np.sqrt(
              (disc.xyzh[0]-disc.ptmass_xyzmh[0,0])**2 +
              (disc.xyzh[1]-disc.ptmass_xyzmh[1,0])**2
@@ -237,7 +274,13 @@ def get_az_averaged_properties(disc,nbins=100,rmax=100):
     return out
 
 def generate_u_udot_file(dump,outfile):
-
+    """
+    Generate data file containing thermodynamic properties of gas disc by running phantom
+        directly. Data file will contain properties not readily available in pyphantom,
+        such as du/dt, ttherm, du/dt (diffusion) and u_equilibrium.
+    Ensure code and the end of the force subroutine in force.F90 is enabled in order
+        to generate the required datafile.
+    """
     # make temporary directory for running all this from
     td = 'tempdir'
     if os.path.exists(td):
@@ -273,7 +316,9 @@ def generate_u_udot_file(dump,outfile):
     return
 
 def read_u_udot_file(infile):
-
+    """
+    Take data file generated in generate_u_udot_file() and parse into usable dictionary.
+    """
     data = np.genfromtxt(infile,dtype=float)
 
     out = {
@@ -294,7 +339,11 @@ def read_u_udot_file(infile):
     return out
 
 def get_az_averaged_u_udot(results_dict,nbins=100,rmax=100):
-
+    """
+    Calculate azimuthally averaged u, du/dt, beta and tcool from values outputted
+        directly from phantom.
+    results_dict should be consistent with that generated in read_u_udot_file().
+    """
     radii = np.sqrt(
         (results_dict['x'] - results_dict['ptmass_x'])**2 +
         (results_dict['y'] - results_dict['ptmass_y'])**2
