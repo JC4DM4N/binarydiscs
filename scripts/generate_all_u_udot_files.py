eb66c849a12ba69ae55f50342e5b64857c9aaa67
James Cadman <cadman@roe.ac.uk>
2021-09-06 14:50:15 +0100

    added script to generate u and du/dt datafiles for all dump files on disc.

---
+++ scripts/generate_all_u_udot_files.py
@@ -0,0 +1,19 @@
+"""
+Run phantom for all dump files on disk to generate u and du/dt datafiles.
+Ensure debugging code in force.F90 is enabled, otherwise the datafile will not 
+	be written.
+"""
+
+import sys
+import os
+import datetime as dt
+
+sys.path.insert(0,'../modules')
+import phantom
+
+all_dumps = phantom.collect_all_dumps()
+
+for dump in all_dumps:
+	fid = phantom.folder_id(dump)
+	outfile = os.path.join('../u_and_udot_files', fid+'.dat')
+	phantom.generate_u_udot_file(dump, outfile)
