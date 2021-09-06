af7554dbc1e55dad13e390e983a6ad05166d8d59
James Cadman <cadman@roe.ac.uk>
2021-09-06 16:55:00 +0100

    bugfix to script

--- scripts/generate_all_u_udot_files.py
+++ scripts/generate_all_u_udot_files.py
@@ -10,9 +10,14 @@ import datetime as dt
 sys.path.insert(0,'../modules')
 import phantom
 
+# ensure outfile directory exists
+outdir = '../u_and_udot_files'
+if not os.path.exists(outdir):
+	os.mkdir(outdir)
+
 all_dumps = phantom.collect_all_dumps()
 
 for dump in all_dumps:
 	fid = phantom.folder_id(dump)
-	outfile = os.path.join('../u_and_udot_files', fid+'.dat')
+	outfile = os.path.join(outdir, fid+'.dat')
 	phantom.generate_u_udot_file(dump, outfile)
