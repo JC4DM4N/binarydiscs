5390682b64283ac903300638a3053c1b4fa10dbc
James Cadman <cadman@roe.ac.uk>
2021-09-28 12:25:16 +0100

    added dictionary of fragment locations for each system which fragmented. Will be used later for making results plots.

---
+++ data/results_dicts.py
@@ -0,0 +1,46 @@
+fragment_locations = {
+    'a250/mdisc0.2' : {
+        'a' = 250,
+        'mdisc' = 0.2,
+        'e' = 0,
+        'i' = 0,
+        'Mstar2' = 0.2,
+        'rperi' = 186,
+        'tfrag' = 3050,
+        'Nfrag' = 2,
+        'rfrag' = [55,60]
+    },
+    'a150/mdisc0.2' : {
+        'a' = 150,
+        'mdisc' = 0.2,
+        'e' = 0,
+        'i' = 0,
+        'Mstar2' = 0.2,
+        'rperi' = 116,
+        'tfrag' = 750,
+        'Nfrag' = 4,
+        'rfrag' = [51,87,98,100]
+    },
+    'a200/mdisc0.2' : {
+        'a' = 200,
+        'mdisc' = 0.2,
+        'e' = 0,
+        'i' = 0,
+        'Mstar2' = 0.2,
+        'rperi' = 150,
+        'tfrag' = 1500,
+        'Nfrag' = 6,
+        'rfrag' = [77,67,103,81,52,55]
+    },
+    'a150/e0.25/mdisc0.2' : {
+        'a' = 150,
+        'mdisc' = 0.2,
+        'e' = 0.25,
+        'i' = 0,
+        'Mstar2' = 0.2,
+        'rperi' = 92,
+        'tfrag' = 900,
+        'Nfrag' = 1,
+        'rfrag' = [54]
+    }
+}
