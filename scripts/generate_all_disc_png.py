a8e38fa28685595fe447db23b273ee63bea3c291
James Cadman <cadman@roe.ac.uk>
2022-02-15 17:19:45 +0000

    scripts/generate_all_disc_png.py now takes orientation of the output image as command line arg.

--- scripts/generate_all_disc_png.py
+++ scripts/generate_all_disc_png.py
@@ -6,11 +6,18 @@ import PIL.Image as Image
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
+import argparse
 
 sys.path.insert(0,os.path.join(os.path.dirname(__file__),'../phantom_files'))
 from libanalysis import PhantomAnalysis as pa
 sys.path.insert(0,os.path.join(os.path.dirname(__file__),'../modules'))
 import phantom
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-orientation', default='xy',
+                    help='orientation of image. xy or xz.')
+args = parser.parse_args()
+orientation = args.orientation
 
 plots_output_dir = '.'
 all_dumps = [file for file in os.listdir('.') if 'abc' in file]
@@ -28,6 +35,6 @@ for dump in all_dumps:
     plot_id = dump + '.png'
     output_fname = os.path.join(plots_output_dir,plot_id)
     # generate and save the plot
-    phantom.generate_png_plot(dump,output_fname)
+    phantom.generate_png_plot(dump,output_fname,orientation)
     # crop the plots to remove white space
     crop_splash_image(output_fname)
