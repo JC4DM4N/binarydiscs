540fe05e6358b878e6fb226ec8292934c1db6fdb
James Cadman <cadman@cuillin.roe.ac.uk>
2022-05-04 17:59:48 +0100

    changes to calc_AM_vector.py - now plotting vector plots of disc and companion angular momentum

--- scripts/calc_AM_vector.py
+++ scripts/calc_AM_vector.py
@@ -1,6 +1,10 @@
 import sys, os
 import argparse
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 sys.path.insert(0,os.path.join(os.path.dirname(__file__),'../phantom_files'))
 from libanalysis import PhantomAnalysis as pa
 sys.path.insert(0,os.path.join(os.path.dirname(__file__),'../modules'))
@@ -50,6 +54,28 @@ if args.include_companion:
             f = open('AM_angles.dat','a')
         f.write("%.2f   %.2f   %.2f   %.2f \n" %(disc.time, theta*180/np.pi, theta2*180/np.pi, theta3*180/np.pi))
         f.close()
+    # also plot the two vectors
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.quiver(
+        0, 0, 0, # <-- starting point of vector
+        AM[0]*10, AM[1]*10, AM[2]*10, # <-- directions of vector
+        color = 'red', alpha = .8, lw = 3, label='disc',
+    )
+    ax.quiver(
+        0, 0, 0, # <-- starting point of vector
+        companion_AM[0], companion_AM[1], companion_AM[2], # <-- directions of vector
+        color = 'blue', alpha = .8, lw = 3, label='companion',
+    )
+    ax.set_xlim([-20,20])
+    ax.set_ylim([-20,20])
+    ax.set_zlim([-20,20])
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    ax.view_init(elev=10., azim=10.)
+    plt.legend()
+    plt.savefig(os.path.join(os.getcwd(),'%s_AM.png' %args.file))
 else:
     # just print results for the disc
     print("Total AM Vector of disc: %.3f %3f %.3f" %(AM[0], AM[1], AM[2]))
